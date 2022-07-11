032fa69d95003430bbdd7a30d47aa277dd5e8661
Paulojoserc <pjrc83@gmail.com>
2022-07-10 21:36:05 -0300

    funções

--- src/_03/estruturacondicional/EscopoInicializacao.java
+++ src/_03/estruturacondicional/EscopoInicializacao.java
@@ -1,17 +1,16 @@
 package _03.estruturacondicional;
 
 public class EscopoInicializacao {
-public static void main(String[] args) {
-	double price = 400.00;
-	
-	// Precisa ser iniciada para usar fora do if
-	double discount =0;
-	
-	if (price < 200.00) {
-		discount = price * 0.1;
+	public static void main(String[] args) {
+		double price = 400.00;
+
+		// Precisa ser iniciada para usar fora do if
+		double discount = 0;
+
+		if (price < 200.00) {
+			discount = price * 0.1;
+		}
+
+		System.out.println(discount);
 	}
-	
-	
-	System.out.println(discount);
-}
 }
