51d613500aeb2239f3a8ae4696f1ce5594b25e6c
Paulojoserc <pjrc83@gmail.com>
2022-08-10 23:57:03 -0300

    Fim do Capitulo de Matriz

--- src/_09/matriz/lab/Diagonal_Negativo.java
+++ src/_09/matriz/lab/Diagonal_Negativo.java
@@ -15,20 +15,49 @@ public static void main(String[] args) {
 			mat[i][j]= sc.nextInt();
 		}
 	}
+	
+	System.out.println();
+	int somaPo=0;
+	for(int i= 0;i<mat.length; i++) {
+		for(int j=0; j< mat[i].length; j++) {
+			if(mat[i][j]<0) {
+				somaPo++;
+			}
+		}
+	}
+	System.out.println("QUANTIDADE DE NEGATIVOS = "+somaPo);
+	System.out.println();
+	System.out.print("Escolha uma linha ");
+	int linha = sc.nextInt();
+	System.out.println("LINHA ESCOLHIDA: ");
+	for(int i=0; i< num; i++) {
+		System.out.printf("%.1f ", mat[linha][i]);
+	}
+	System.out.printf("\n\nEscolha uma coluna: ");
+	int coluna = sc.nextInt();
+	System.out.println("COLUNA ESCOLHIDA: ");
+	for(int i=0; i< num; i++) {
+		System.out.printf("%.1f ", mat[coluna][i]);
+	}
 	System.out.println("DIAGONAL PRINCIPAL:");
 	for(int i =0; i<mat.length; i++) {
 		System.out.print(mat[i][i]+" ");
 	}
-	System.out.println();
-	int cont=0;
-	for(int i= 0;i<mat.length; i++) {
-		for(int j=0; j< mat[i].length; j++) {
-			if(mat[i][j]<0) {
-				cont++;
+	for(int i= 0;i<num; i++) {
+		for(int j=0; j< num; j++) {
+			if(mat[i][j]<0.0) {
+				mat[i][j] =  (int) Math.pow(mat[i][j], 2);
 			}
+			
 		}
 	}
-	System.out.println("QUANTIDADE DE NEGATIVOS = "+cont);
+	System.out.println("Matriz Alterada:");
+	for(int i= 0;i<mat.length; i++) {
+		for(int j=0; j< mat[i].length; j++) {
+			System.out.printf("%.1f ", mat[i][j]);
+		}
+		System.out.println();
+	}
 	
 	sc.close();
 }
--- src/_09/matriz/lab/Matriz_Geral.java
+++ src/_09/matriz/lab/Matriz_Geral.java
@@ -7,23 +7,61 @@ public class Matriz_Geral {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double somaPositivo =0.0;
+		double somaPositivo = 0.0;
+
 		System.out.print("Qual a ordem da matriz? ");
 		int num = sc.nextInt();
 
 		double[][] mat = new double[num][num];
 
 		for (int i = 0; i < num; i++) {
-			for (int j = 0; j <num; j++) {
+			for (int j = 0; j < num; j++) {
 				System.out.print("Elemento [" + i + "," + j + "]: ");
 				mat[i][j] = sc.nextDouble();
-				if( mat[i][j] > 0) {
+				if (mat[i][j] > 0) {
 					somaPositivo += mat[i][j];
 				}
 			}
 		}
-		System.out.print("SOMA DOS POSITIVOS: "+somaPositivo);
+		System.out.print("SOMA DOS POSITIVOS: " + somaPositivo);
+		System.out.println();
+		System.out.print("Escolha uma linha: ");
+		int linha = sc.nextInt();
+		System.out.print("LINHA ESCOLHIDA: ");
+		for (int i = 0; i < num; i++) {
+			System.out.print(mat[linha][i]+" ");
+		}
+		System.out.println();
+		System.out.println();
+		System.out.print("Escolha uma coluna: ");
+		int coluna = sc.nextInt();
+		System.out.print("COLUNA ESCOLHIDA: ");
+		for (int i = 0; i < num; i++) {
+			System.out.print(mat[i][coluna]+" ");
+		}
+		System.out.println();
+		System.out.println();
+		System.out.print("DIAGONAL PRINCIPAL: ");
+		for (int i = 0; i < num; i++) {
+			System.out.print(mat[i][i]+" ");
+		}
+		System.out.println();
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num; j++) {
+				if (mat[i][j] < 0) {
 
+					mat[i][j] = Math.pow(mat[i][j], 2);
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("Matriz ALTERNADA: ");
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num; j++) {
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
 		sc.close();
 	}
 }
