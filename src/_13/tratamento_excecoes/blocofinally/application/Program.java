611008234cbe73559999c6a47cef723d1dd50975
Paulo Costa <pjrc83@gmail.com>
2022-08-23 17:41:40 -0300

    Try Exemplo correto

---
+++ src/_13/tratamento_excecoes/blocofinally/application/Program.java
@@ -0,0 +1,25 @@
+package _13.tratamento_excecoes.blocofinally.application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		File file = new File("D:\\usr\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error openung file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
+		}
+	}
+}
---
+++ src/_13/tratamento_excecoes/projeto/model/excepTions/DomainException.java
@@ -0,0 +1,10 @@
+package _13.tratamento_excecoes.projeto.model.excepTions;
+
+public class DomainException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String msg) {
+		
+	}
+}
