f89e07c74519ea68174294f607433ead16dba6b0
Paulo Costa <pjrc83@gmail.com>
2022-09-13 17:25:16 -0300

    primeira ordem

--- src/_17/funcional_lambda/comparator_com_funcao/application/Program.java
+++ src/_17/funcional_lambda/comparator_com_funcao/application/Program.java
@@ -15,10 +15,10 @@ public class Program {
 		list.add(new Product("Tablet", 450.00));
 		
 		//Comparator<Product> comp = (p1, p2) -> {
-			//return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			//return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); 3) resumida 
 		//};
 			
-			//*new Comparator<Product>() {
+			//*new Comparator<Product>() { 2)modelo não resumido 
 			//@Override
 			//public int compare(Product p1, Product p2) {
 			//return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
@@ -27,7 +27,7 @@ public class Program {
 		
 		list.sort((p1, p2)->p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
-		//list.sort(new MyComparator());
+		//list.sort(new MyComparator()); 1) dependendo da classe MyComparator
 		
 		for (Product p : list) {
 			System.out.println(p);
